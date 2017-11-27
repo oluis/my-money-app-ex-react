2b8bc0162cf9f892a52d5f7d930cec82de098672
luis.loureiro <luis.loureiro@softplan.com.br>
2017-11-27 20:45:12 -0200

    criado servicos do backend e configuracoes iniciais

---
+++ backend/src/config/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+module.exports = function(server){
+  //Defini URL base para todas as chamadas
+  const router = express.Router()
+  server.use('/api', router)
+
+  //Rotas
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(router, '/billingCycles')
+
+}
