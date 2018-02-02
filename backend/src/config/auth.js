0d404b727cfb301d0034795575f8cca1cb6efe5d
Luis Osorio <luis.loureiro@softplan.com.br>
2018-02-01 22:03:24 -0200

    implementado autenticacao e tela de login

---
+++ backend/src/config/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const env = require('../.env')
+
+module.exports = (req, res, next) => {
+  // CORS config
+  if (req.method === 'OPTIONS') {
+    next()
+  } else {
+    const token = req.body.token || req.query.token || req.headers['authorization']
+    if (!token) {
+      return res.status(403).send({ errors: ['Nenhum token encontrado'] })
+    }
+    jwt.verify(token, env.authSecret, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({ errors: ['Falha ao validar token.'] })
+      } else {
+        //req.decoded = decoded
+        next()
+      }
+    })
+  }
+}
--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -5,10 +5,11 @@ import {Router, Route, IndexRoute, Redirect, hashHistory} from 'react-router'
 import App from './app'
 import Dashboard from '../dashboard2/dashboardSemRedux2'
 import BillingCycle from '../billingCycle/billingCycle'
+import AuthOrApp from './authOrApp'
 
 export default props => (
   <Router history={hashHistory}>
-    <Route path='/' component={App}>
+    <Route path='/' component={AuthOrApp}>
       <IndexRoute component={Dashboard} />
       <Route path='billingCycles' component={BillingCycle} />
     </Route>
